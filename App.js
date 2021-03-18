dafd8b4a65efefef21e85411bd40d96dbd277260
ronnie <ronnie@freehitnews.com>
2021-03-18 19:46:36 +0530

    added navigation

--- App.js
+++ App.js
@@ -1,88 +1,21 @@
-import React, { useEffect, useState, useRef } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  Dimensions,
-  Image,
-} from 'react-native';
-import Recipes from './src/Data/Recipes';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import DetailScreen from './src/screens/DetailScreen';
 
-const { width, height } = Dimensions.get('window');
+const Stack = createStackNavigator();
+
 export default function App() {
-  const ref = useRef({ scroll: null });
-
-  const renderItem = ({ item }) => {
-    console.log(item);
-    const { label, raiting, cooking_time, calories, image } = item.recipe;
-    return (
-      <View style={styles.recipeContainer}>
-        <View style={styles.recipeTitleContainer}>
-          <Image
-            source={{ uri: image }}
-            style={{
-              width: width * 0.8,
-              height: height * 0.5,
-              alignSelf: 'center',
-              borderRadius: width,
-              resizeMode: 'cover',
-            }}
-          />
-          <Text style={{ fontSize: 32 }}>{label}</Text>
-          <Text>Raiting {raiting}</Text>
-        </View>
-        <View style={styles.recipeInfoContainer}>
-          <Text style={styles.text}>Calories {calories}</Text>
-          <Text style={styles.text}>Cooking Time {cooking_time}</Text>
-        </View>
-      </View>
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <FlatList
-        ref={(r) => (ref.current.scroll = r)}
-        data={Recipes}
-        ListEmptyComponent={() => <Text>No Data</Text>}
-        renderItem={renderItem}
-        pagingEnabled
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height,
-    width,
-    backgroundColor: '#fff',
-  },
-  recipeContainer: {
-    backgroundColor: '#e3b505',
-    borderWidth: 1,
-    width,
-    height,
-    flexGrow: 1,
-  },
-  recipeTitleContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: height * 0.9,
-  },
-  recipeInfoContainer: {
-    backgroundColor: '#000',
-    position: 'absolute',
-    bottom: 0,
-    width,
-    flexDirection: 'row',
-    height: height * 0.1,
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  text: {
-    color: '#fff',
-  },
-  imageContainer: {},
-});
